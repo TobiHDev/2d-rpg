f0fa802fa2c1fc4f9748ac970b99dd9df1644cf6
thuahuy <thuahuy.devgame@gmail.com>
2025-10-05 16:30:29 +0700

    added some animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,11 +6,25 @@ public class Player : MonoBehaviour
     public StateMachine stateMachine { get; private set; }
     public Player_IdleState idleState{ get; private set; }
     public Player_MoveState moveState{ get; private set; }
+    private PlayerInputSet input;
+    public Vector2 mouseInput { get; private set; }
+
     private void Awake()
     {
         stateMachine = new StateMachine();
         idleState = new Player_IdleState(this, stateMachine, "Idle");
         moveState = new Player_MoveState(this, stateMachine, "Move");
+        input = new PlayerInputSet();
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += ctx => mouseInput = ctx.ReadValue<Vector2>();
+        input.Player.Movement.canceled += ctx => mouseInput = Vector2.zero;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
     }
     private void Start()
     {
@@ -18,6 +32,6 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
-        stateMachine.currentState.Update();
+        stateMachine.UpdateActiveState();
     }
 }
